4e0ed59be2635669cb1d04720af034309a6ceba2
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-04-19 16:42:26 +0200

    Delete 'adidas.sh' script from gitignore file

--- adidas.sh
+++ adidas.sh
@@ -1,11 +1,11 @@
 cd /home/ruben/GitHub/arquitectura-software/src/web/
-CLASSPATH='/home/ruben/GitHub/arquitectura-software/apache-tomcat-7.0.68/lib/servlet-api.jar:/home/ruben/GitHub/arquitectura-software/json-lib.jar:/home/ruben/GitHub/arquitectura-software/mysql-connector-java-5.1.38-bin.jar' javac servlets/*.java database/conection/*.java database/dataAccessObject/*.java database/valueObject/*.java
+CLASSPATH='/home/ruben/GitHub/arquitectura-software/apache-tomcat-7.0.68/lib/servlet-api.jar:/home/ruben/GitHub/arquitectura-software/json-lib.jar:/home/ruben/GitHub/arquitectura-software/mysql-connector-java-5.1.38-bin.jar' javac servlets/*.java database/conection/*.java database/dataAccessObject/*.java database/valueObject/*.java utils/*.java
 
 mv /home/ruben/GitHub/arquitectura-software/src/web/servlets/*.class /home/ruben/GitHub/arquitectura-software/redSocial/WEB-INF/classes/web/servlets
 mv /home/ruben/GitHub/arquitectura-software/src/web/database/conection/*.class /home/ruben/GitHub/arquitectura-software/redSocial/WEB-INF/classes/web/database/conection
 mv /home/ruben/GitHub/arquitectura-software/src/web/database/dataAccessObject/*.class /home/ruben/GitHub/arquitectura-software/redSocial/WEB-INF/classes/web/database/dataAccessObject
 mv /home/ruben/GitHub/arquitectura-software/src/web/database/valueObject/*.class /home/ruben/GitHub/arquitectura-software/redSocial/WEB-INF/classes/web/database/valueObject
-
+mv /home/ruben/GitHub/arquitectura-software/src/web/utils/*.class /home/ruben/GitHub/arquitectura-software/redSocial/WEB-INF/classes/web/utils
 
 jar -cvf /home/ruben/GitHub/arquitectura-software/redSocial/redSocial.war -C /home/ruben/GitHub/arquitectura-software/redSocial/ .
 
